b578004ae05f056df4706c87d262d5a58d65691b
kkopanidis <kkopanidis@gmail.com>
2016-03-14 19:49:36 +0200

    forgotten folder added

---
+++ HaiFisch/src/com/haifisch/server/reduce/utils/Serialize.java
@@ -0,0 +1,21 @@
+package com.haifisch.server.reduce.utils;
+
+import java.io.*;
+
+public class Serialize {
+
+    public static byte[] serialize(Object obj) throws IOException {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutput out = new ObjectOutputStream(bos)) {
+            out.writeObject(obj);
+            return bos.toByteArray();
+        }
+    }
+
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+             ObjectInput in = new ObjectInputStream(bis)) {
+            return in.readObject();
+        }
+    }
+}
