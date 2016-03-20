4199aa788fc47eb68af74c06233fb519727d8dea
p3130171 <p3130171@dias.aueb.gr>
2016-03-20 19:55:03 +0200

    Added files via upload
    Coordinates for a point on a map.

---
+++ HaiFisch/src/com/haifisch/server/map/Coordinates.java
@@ -0,0 +1,55 @@
+package com.haifisch.server.map;
+
+/** Class that represents a point on a map. */
+public class Coordinates {
+
+	public int longtitude;
+	public int latitude;
+	
+	/**
+	 * Constructor.
+	 * @param longtitude
+	 * @param latitude
+	 */
+	public Coordinates(int longtitude, int latitude) {
+	
+		this.longtitude = longtitude;
+		this.latitude = latitude;
+	}
+	
+	/**
+	 * Sets the longtitude of a map coordinate.
+	 * @param longtitude
+	 */
+	public void setLongtitude(int longtitude) {
+
+		this.longtitude = longtitude;
+	}
+	
+	/**
+	 * Sets the latitude of a map coordinate.
+	 * @param latitude
+	 */
+	public void setLatitude(int latitude) {
+
+		this.latitude = latitude;
+	}
+	
+	/**
+	 * Gets the longtitude of a map coordinate.
+	 * @return longtitude
+	 */
+	public int getLongtitude() {
+
+		return longtitude;
+	}
+	
+	/**
+	 * Gets the latitude of a map coordinate.
+	 * @return latitude
+	 */
+	public int getLatitude() {
+
+		return latitude;
+	}
+}
