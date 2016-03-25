7351312d3bf27d7f5ad57ecd9c65847e94970907
ολγα <@SHELDON>
2016-03-25 13:38:56 +0200

    new classes

---
+++ HaiFisch/src/com/haifisch/server/map/Map_Server.java
@@ -0,0 +1,16 @@
+package com.haifisch.server.map;
+
+public class Map_Server {
+
+	public static void  main(String[] args){
+		
+		//create a listening socket
+		
+		//serving socket results
+		
+		//manager (Request_Manager) thread activated 
+			
+		
+		
+	}
+}
---
+++ HaiFisch/src/com/haifisch/server/map/Request_Manager.java
@@ -0,0 +1,17 @@
+package com.haifisch.server.map;
+
+public class Request_Manager implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//---> starts a Mapper thread
+		//---> waits for join
+		//---> getResults()
+		//---> Sending Sockets sends the results to reducer
+		//---> sends "JOB DONE" to master server
+	}
+	
+	
+	
+}
