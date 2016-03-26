99cc62f5167780359efbf7e0b46879ba615e9446
kkopanidis <kkopanidis@gmail.com>
2016-03-26 15:28:34 +0200

    Incoming v3

--- HaiFisch/src/com/haifisch/server/map/Map_Server.java
+++ HaiFisch/src/com/haifisch/server/map/Map_Server.java
@@ -1,16 +1,47 @@
 package com.haifisch.server.map;
 
+import com.haifisch.server.utils.Questionaire;
+import com.haifisch.server.utils.Configuration;
+import com.haifisch.server.utils.Point;
+
+
 public class Map_Server {
 
-	public static void  main(String[] args){
-		
-		//create a listening socket
-		
-		//serving socket results
-		
-		//manager (Request_Manager) thread activated 
-			
-		
-		
+	public static void main(String args[]) {
+
+		//Create the object that will get all the input information from the user.
+		Questionaire q = new Questionaire();
+
+		//Create the points on the map represented by the given coordinates.
+		Point topLeftPoint = new Point (q.topLeftCoordinateLongitude, q.topLeftCoordinateLatitude);
+		Point bottomRightPoint = new Point (q.bottomRightCoordinateLongtitude, q.bottomRightCoordinateLatitude);
+
+		//Calculate the other two points of the map.
+		Point bottomLeftPoint = new Point(topLeftPoint.longtitude, bottomRightPoint.latitude);
+		Point topRightPoint = new Point(bottomRightPoint.longtitude, topLeftPoint.latitude);
+
+		topRightPoint.print(); //DEBUG
+		bottomLeftPoint.print(); //DEBUG
+
+		//Create a new configuration object for the master server.
+		Configuration MasterConfig = new Configuration(q.masterServerName, q.masterServerPort);
+
+		//Create a new configuration object for all the mappers.
+		MapperConfiguration MapperConfig = new
+				MapperConfiguration(q.masterServerName, q.masterServerPort, q.reducerName, q.reducerPort,
+				topLeftPoint, bottomRightPoint);
+
+
+		//WAITING IMPLEMENTATION....
+
+		//Create a new configuration object for the reducer.
+		//ReducerConfiguration ReducerConfig = new ReducerConfiguration();
+
+		//END WAITING IMPLEMENTATION.
+
+
+
 	}
+
+}
 }
---
+++ HaiFisch/src/com/haifisch/server/reduce/MainProgram.java
@@ -0,0 +1,49 @@
+package com.haifisch.server.reduce;
+
+import com.haifisch.server.utils.Questionaire;
+import com.haifisch.server.map.MapperConfiguration;
+import com.haifisch.server.utils.Point;
+import com.haifisch.server.utils.Configuration;
+
+
+public class MainProgram {
+
+	public static void main(String args[]) {
+		
+		//Create the object that will get all the input information from the user.
+		Questionaire q = new Questionaire();
+
+		//Create the points on the map represented by the given coordinates.
+		Point topLeftPoint = new Point (q.topLeftCoordinateLongitude, q.topLeftCoordinateLatitude);
+		Point bottomRightPoint = new Point (q.bottomRightCoordinateLongtitude, q.bottomRightCoordinateLatitude);
+		
+		//Calculate the other two points of the map.
+		Point bottomLeftPoint = new Point(topLeftPoint.longtitude, bottomRightPoint.latitude);
+		Point topRightPoint = new Point(bottomRightPoint.longtitude, topLeftPoint.latitude);
+		
+		topRightPoint.print(); //DEBUG
+		bottomLeftPoint.print(); //DEBUG
+		
+		//Create a new configuration object for the master server.
+		Configuration MasterConfig = new Configuration(q.masterServerName, q.masterServerPort);
+		
+		//Create a new configuration object for all the mappers.
+		MapperConfiguration MapperConfig = new 
+		MapperConfiguration(q.masterServerName, q.masterServerPort, q.reducerName, q.reducerPort, 
+		topLeftPoint, bottomRightPoint);
+		
+		
+		//WAITING IMPLEMENTATION....
+		
+			//Create a new configuration object for the reducer.
+			//ReducerConfiguration ReducerConfig = new ReducerConfiguration();
+		
+		//END WAITING IMPLEMENTATION.
+		
+		
+		
+		
+
+	}
+
+}
