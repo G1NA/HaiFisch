c12dab2e8d5beb482947282474f9bd5e7a0fd26f
kkopanidis <kkopanidis@gmail.com>
2016-03-29 12:26:49 +0300

    DB ip updated

--- HaiFisch/src/com/haifisch/server/map/Mapper.java
+++ HaiFisch/src/com/haifisch/server/map/Mapper.java
@@ -23,7 +23,7 @@ public class Mapper implements Runnable {
     @Override
     public void run() {
 
-        DatabaseManager db = new DatabaseManager("jdbc:mysql://195.251.252.98/ds_systems_2016?user=omada26&password=omada26db");
+        DatabaseManager db = new DatabaseManager("jdbc:mysql:// 83.212.117.76:3306/ds_systems_2016?user=omada26&password=omada26db");
 
         db.connectToDatabase();
 
