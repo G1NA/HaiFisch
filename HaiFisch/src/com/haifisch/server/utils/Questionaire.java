728adca1811a70d62a4426b924dcdbe54fde8af9
p3130171 <p3130171@dias.aueb.gr>
2016-04-02 21:36:54 +0300

    Removed the idiot method.

--- HaiFisch/src/com/haifisch/server/utils/Questionaire.java
+++ HaiFisch/src/com/haifisch/server/utils/Questionaire.java
@@ -12,13 +12,7 @@ public class Questionaire {
     //---> ta evala public alla mporoume na valoume kai getters
     public final String masterServerName;
     public final int masterServerPort;
-    public final String reducerName;
-    public final int reducerPort;
-    public final double topLeftCoordinateLongitude;
-    public final double topLeftCoordinateLatitude;
-    public final double bottomRightCoordinateLongtitude;
-    public final double bottomRightCoordinateLatitude;
-
+  
     /**
      * Set the master server 's name.
      *
@@ -42,73 +36,6 @@ public class Questionaire {
     }
 
     /**
-     * Set the reducer 's name.
-     *
-     * @return name
-     */
-    private String setReducerName() {
-
-        System.out.println("Insert reducer name:");
-        return scanner.nextLine();
-    }
-
-    /**
-     * Set the reducer 's port.
-     *
-     * @return port
-     */
-    private int setReducerPort() {
-
-        System.out.println("Insert reducer port:");
-        return Integer.parseInt(scanner.nextLine().trim());
-    }
-
-    /**
-     * Set the top left point 's longtitude.
-     *
-     * @return value
-     */
-    private Double setTopLeftLongtitude() {
-
-        System.out.println("Enter top left point 's longtitude:");
-        return Double.parseDouble(scanner.nextLine().trim());
-    }
-
-    /**
-     * Set the top left point 's latitude.
-     *
-     * @return value
-     */
-    private Double setTopLeftLatitude() {
-
-        System.out.println("Enter top left point 's latitude:");
-        return Double.parseDouble(scanner.nextLine().trim());
-    }
-
-
-    /**
-     * Set the bottom right point 's longtitude.
-     *
-     * @return value
-     */
-    private Double setBottomRightLongtitude() {
-
-        System.out.println("Enter bottom right point 's longtitude:");
-        return Double.parseDouble(scanner.nextLine().trim());
-    }
-
-    /**
-     * Set the bottom right point 's latitude.
-     *
-     * @return value
-     */
-    private Double setBottomRightLatitude() {
-
-        System.out.println("Enter bottom right point 's latitude:");
-        return Double.parseDouble(scanner.nextLine().trim());
-    }
-
-    /**
      * Constructor.
      */
     public Questionaire() {
@@ -117,15 +44,6 @@ public class Questionaire {
 
         masterServerName = setMasterServerName();
         masterServerPort = setMasterServerPort();
-        reducerName = setReducerName();
-        reducerPort = setReducerPort();
-        topLeftCoordinateLongitude = setTopLeftLongtitude();
-
-        topLeftCoordinateLatitude = setTopLeftLatitude();
-
-        bottomRightCoordinateLongtitude = setBottomRightLongtitude();
-
-        bottomRightCoordinateLatitude = setBottomRightLatitude();
 
     }
 }
