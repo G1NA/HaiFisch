6731f08f32d8da1229bef649aebf75f95557fd12
p3130171 <p3130171@CSLAB1-54>
2016-04-04 17:02:32 +0300

    hahaha

--- HaiFisch/src/com/haifisch/server/map/Map_Server.java
+++ HaiFisch/src/com/haifisch/server/map/Map_Server.java
@@ -5,14 +5,17 @@ import com.haifisch.server.NetworkTools.NetworkPayload;
 import com.haifisch.server.NetworkTools.onConnectionListener;
 import com.haifisch.server.utils.Questionaire;
 
-
+/** Basic class responsible for the actions executed by the server node. */
 public class Map_Server extends MainProgram implements onConnectionListener {
 
     public volatile static Map_Server server;
     private final MapperConfiguration configuration;
 
+   /** 
+     * The main method of the server node class. 
+     * Responsible for the initialization of the server configuration.
+    */
     public static void main(String args[]) {
-
 
         //Create the object that will get all the input information from the user.
         Questionaire q = new Questionaire();
@@ -24,19 +27,29 @@ public class Map_Server extends MainProgram implements onConnectionListener {
         server.toolsInit();
 
     }
-
+    
+   /** 
+	 * Constructor.
+	 * Creates a listening socket and connects to the master server.
+	 * @param configuration	The configuration of the mapper asking to connect with the server.
+	 */
     private Map_Server(MapperConfiguration configuration) {
         this.configuration = configuration;
         createListeningSocket(); //Create the listening socket.
         connectToMaster(configuration.masterServerName, configuration.masterServerPort, 1);
     }
 
+   /** 
+     * Creates a new thread for the request handler.
+     * @param payload A payload that contains the basic characteristics of the connection. 
+     */
     @Override
     synchronized public void onConnect(NetworkPayload payload) {
         System.out.println("Serving new request from: " + payload.SENDER_NAME);
         new Thread(new RequestHandler(payload)).start();
     }
-
+    
+    /** Getter method. Returns the configuration of the mapper. */
     synchronized public MapperConfiguration getConfiguration() {
         return configuration;
     }
--- HaiFisch/src/com/haifisch/server/utils/Configuration.java
+++ HaiFisch/src/com/haifisch/server/utils/Configuration.java
@@ -1,9 +1,14 @@
 package com.haifisch.server.utils;
 
+/** Class that represents the basic configuration for the communication with the server. */
+
 public class Configuration {
+	
     public final String masterServerName;
     public final int masterServerPort;
-
+    
+    
+    /** Constructor */
     public Configuration(String masterServerName, int masterServerPort) {
         this.masterServerName = masterServerName;
         this.masterServerPort = masterServerPort;
