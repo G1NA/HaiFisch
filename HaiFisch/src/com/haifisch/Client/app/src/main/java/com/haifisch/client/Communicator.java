6ba87a5f2e05f12a5470415d482519aad3c5c669
kkopanidis <kkopanidis@gmail.com>
2016-06-20 20:36:20 +0300

    re-organizing

--- HaiFisch/src/com/haifisch/Client/app/src/main/java/com/haifisch/client/Communicator.java
+++ HaiFisch/src/com/haifisch/Client/app/src/main/java/com/haifisch/client/Communicator.java
@@ -51,6 +51,7 @@ public class Communicator extends AsyncTask {
 
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
+                return null;
             }
 
         }
